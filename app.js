22d70e53e7e47a559aa4883ec995206683bb4972
Lucas Herrera <lucasherrera@Lucass-MacBook-Air.local>
2024-01-25 14:36:36 -0700

    Update js html file

--- app.js
+++ app.js
@@ -18,20 +18,40 @@ async function getData() {
             api_key : "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym"
         }
     });
-    console.log(res);
+    // Check if the data array has items
+    if (res.data.data.length > 0) {
+        // Generate a random index based on the length of the data array
+        let randomIdx = Math.floor(Math.random() * res.data.data.length);
+
+        // Pass the GIF at the random index to the makeMeme function
+        makeMeme(res.data.data[randomIdx]);
+    } else {
+        console.log("No GIFs found for the search term.");
+    }
 }
 
+
+
 function makeMeme (gifData) {
-    
+    const imageUrl = gifData.url
     const newImg = document.createElement('img');
-    newImg.src = gifData.images.original.url;
+    newImg.classList.add('img-style')
+    newImg.src = imageUrl
     imgDiv.appendChild(newImg)
 }
 
 const btn = document.querySelector(".submit-meme");
-btn.addEventListener('click', makeMeme)
+btn.addEventListener('click', getData)
 // 6. Grab a gif from reponse data
 // 7. create a img element in the div that the images should be in
 // 8. Append the giv from reponse data variable to the img inside the div.
 // 9. Add event listener to the remove button that when clicked it 
 // 10. make function that has the remove method in the dom.
+
+const removeBtn = document.querySelector('.remove-memes')
+    removeBtn.addEventListener('click', () => {
+        const myNodeList = [...document.getElementsByTagName('img')]
+        myNodeList.forEach((item)=> item.parentNode.removeChild(item))
+    
+    })
+
